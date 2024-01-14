2d6a70cdeb9626567305e50ac09cf7f8afb75dc6
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2024-01-14 21:34:50 +0700

    pull

--- security/src/main/java/com/example/security/entity/CustomerAddress.java
+++ security/src/main/java/com/example/security/entity/CustomerAddress.java
@@ -1,14 +1,14 @@
 package com.example.security.entity;
 
 import com.example.common.entity.EntityBase;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -34,19 +34,5 @@ public class CustomerAddress extends EntityBase {
     private String Receiver;
     @Column(name = "full_address")
     private String FullAddress;
-    @PrePersist
-    protected void onCreate() {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        LocalDateTime formattedNow = LocalDateTime.parse(now.format(formatter));
-        setCreatedDate(formattedNow);
-        setModifiedDate(formattedNow);
-    }
-    @PreUpdate
-    protected void onUpdate() {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String formattedNow = now.format(formatter);
-        setModifiedDate(formattedNow);
-    }
+    
 }
