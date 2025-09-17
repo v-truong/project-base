67f1ffa8bddf7d2e7264608a4f9fbdf6c99a3647
Truong Nguyen Van <truongdev203@gmail.com>
2025-09-17 14:34:14 +0700

    dd

--- security/src/main/java/com/example/security/repo/jdbd/createSQL.java
+++ security/src/main/java/com/example/security/repo/jdbd/createSQL.java
@@ -1,4 +1,71 @@
-package com.example.security.repo.jdbd;
+package com.tpb.tpb_agency_admin.repo.jdbc;
 
-public class createSQL {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@AllArgsConstructor
+public class RoleProcedure {
+    private final JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void init() {
+        createPackageSpec();
+        createPackageBody();
+    }
+
+    private void createPackageSpec() {
+        String packageSpec = "BEGIN\n" +
+                " EXECUTE IMMEDIATE '\n" +
+                " CREATE OR REPLACE PACKAGE ROLE_ADVANCE AS\n" +
+                " PROCEDURE get_employees_by_dept(\n" +
+                " p_result OUT SYS_REFCURSOR,\n" +
+                " p_fulltextsearch VARCHAR2,\n" +
+                " p_roleId VARCHAR2,\n" +
+                " p_roleName VARCHAR2,\n" +
+                " p_status VARCHAR2,\n" +
+                " p_roleGroup VARCHAR2,\n" +
+                " p_page INT,\n" +
+                " p_size INT\n" +
+                " );\n" +
+                " END ROLE_ADVANCE;';\n" +
+                " EXCEPTION\n" +
+                " WHEN OTHERS THEN IF SQLCODE != -955 THEN RAISE; END IF;\n" +
+                "END;";
+        jdbcTemplate.execute(packageSpec);
+    }
+
+    private void createPackageBody() {
+        String packageBody = "BEGIN\n" +
+                " EXECUTE IMMEDIATE '\n" +
+                " CREATE OR REPLACE PACKAGE BODY ROLE_ADVANCE AS\n" +
+                " PROCEDURE get_employees_by_dept(\n" +
+                " p_result OUT SYS_REFCURSOR,\n" +
+                " p_fulltextsearch VARCHAR2,\n" +
+                " p_roleId VARCHAR2,\n" +
+                " p_roleName VARCHAR2,\n" +
+                " p_status VARCHAR2,\n" +
+                " p_roleGroup VARCHAR2,\n" +
+                " p_page INT,\n" +
+                " p_size INT\n" +
+                " ) IS\n" +
+                " BEGIN\n" +
+                " OPEN p_result FOR\n" +
+                " SELECT * FROM ROLE\n" +
+                " WHERE\n" +
+                " (p_roleId IS NULL OR ROLE_ID LIKE p_roleId)\n" +
+                " AND (p_roleName IS NULL OR ROLE_NAME LIKE p_roleName)\n" +
+                " AND (p_status IS NULL OR STATUS = p_status)\n" +
+                " AND (p_roleGroup IS NULL OR ROLE_GROUP = p_roleGroup)\n" +
+                " OFFSET NVL(p_page, 0) ROWS FETCH NEXT NVL(p_size, 10) ROWS ONLY;\n" +
+                " END get_employees_by_dept;\n" +
+                " END ROLE_ADVANCE;';\n" +
+                " EXCEPTION\n" +
+                " WHEN OTHERS THEN IF SQLCODE != -955 THEN RAISE; END IF;\n" + "END;";
+        jdbcTemplate.execute(packageBody);
+    }
 }
--- security/src/main/java/com/example/security/repo/jdbd/test.java
+++ security/src/main/java/com/example/security/repo/jdbd/test.java
@@ -1,4 +1,34 @@
 package com.example.security.repo.jdbd;
 
 public class test {
+    public List<Role> getRolesByDepartment(RoleSearchRequest request) {
+        String sql = "{ call ROLE_ADVANCE.get_employees_by_dept(?, ?, ?, ?, ?, ?, ?, ?) }";
+
+        List<Role> roles = new ArrayList<>();
+
+        jdbcTemplate.execute((ConnectionCallback<Void>) connection -> {
+            try (CallableStatement cs = connection.prepareCall(sql)) {
+                cs.registerOutParameter(1, OracleTypes.CURSOR);
+                cs.setString(2, request.getFulltext());
+                cs.setString(3, request.getRoleId());
+                cs.setString(4, request.getRoleName());
+                cs.setString(5, request.getStatus());
+                cs.setString(6, request.getRoleGroup());
+                cs.setInt(7, request.getPage());
+                cs.setInt(8, request.getSize());
+
+                cs.execute();
+
+                try (ResultSet rs = (ResultSet) cs.getObject(1)) {
+                    while (rs.next()) {
+                        roles.add(mapResultSetToRole(rs));
+                    }
+                }
+            }
+            return null;
+        });
+
+        return roles;
+    }
+
 }
